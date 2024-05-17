488eb513ef7dd2a0d57b0fbe4f162dbcd8050a93
sonpham28052002 <sonpham28052002@gmail.com>
2024-05-17 15:51:17 +0700

    fix properties

--- UserAuthenticationService/src/main/java/com/example/userauthenticationservice/UserAuthenticationServiceApplication.java
+++ UserAuthenticationService/src/main/java/com/example/userauthenticationservice/UserAuthenticationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.userauthenticationservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/LichHocService", description = "Default Server URL")})
 public class UserAuthenticationServiceApplication {
 
     public static void main(String[] args) {
