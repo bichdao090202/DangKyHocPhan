042ff60c7a5d5ccc34f9a452f85cbd93c044ae37
sonpham28052002 <sonpham28052002@gmail.com>
2024-05-17 16:08:36 +0700

    add UserAuthenticationService into gateway and server euruka

--- UserAuthenticationService/src/main/java/com/example/userauthenticationservice/UserAuthenticationServiceApplication.java
+++ UserAuthenticationService/src/main/java/com/example/userauthenticationservice/UserAuthenticationServiceApplication.java
@@ -2,13 +2,15 @@ package com.example.userauthenticationservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(servers = {@Server(url = "/LichHocService", description = "Default Server URL")})
 public class UserAuthenticationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserAuthenticationServiceApplication.class, args);
     }
